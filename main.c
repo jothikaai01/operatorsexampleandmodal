ebe1fc3c76d1e605f6de50bb90fa3fc7fc8b53aa
jothikajai01 <jothikajai01@gmail.com>
2023-12-28 13:34:37 +0530

    main added

---
+++ main.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //addition();
+    //subraction();
+    //multiplication();
+    //division();
+    //modulas();
+    increment();
+    return 0;
+}
