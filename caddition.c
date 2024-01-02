68ef484b268217f7cc1db6bbe76a4598f9c865fc
jothikaai01 <154311625+jothikaai01@users.noreply.github.com>
2024-01-02 13:20:43 +0530

    Create caddition.c
    democprogram

---
+++ caddition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int addition()
+{
+    int tamil=50,english=50,maths=50,total;
+    total=tamil+english+maths;
+    printf("ADDITION\n");
+    printf("---------\n");
+    printf("tamil=%d\nenglish=%d\nmaths=%d\ntotal=%d",tamil,english,maths,total);
+    return 0;
+
+}
